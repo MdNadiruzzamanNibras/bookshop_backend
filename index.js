15e025d322c0ed4acd275456bbafe72ef780f874
MdNadiruzzamanNibras <nadiruzzamannibras@gmail.com>
2023-07-31 20:17:50 +0600

    change the port

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const express = require('express')
+const cors = require('cors');
 require('dotenv').config();
 const app = express()
-const port = 5000 || process.env.PORT
+ const port = process.env.PORT || 5000;
+
 const { MongoClient, ServerApiVersion, ObjectId} = require('mongodb');
-const cors = require('cors');
+
 
 app.use(cors());
 app.use(express.json());
