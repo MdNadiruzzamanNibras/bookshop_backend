938cc92012be9688ec2bc9592e3f5d80f590aaf2
MdNadiruzzamanNibras <nadiruzzamannibras@gmail.com>
2023-07-31 11:42:46 +0600

    vercel json[done]

--- index.js
+++ index.js
@@ -2,11 +2,14 @@ const express = require('express')
 require('dotenv').config();
 const app = express()
 const port = 5000
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId} = require('mongodb');
+const cors = require('cors');
 
+app.use(cors());
+app.use(express.json());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.qrmeula.mongodb.net/?retryWrites=true&w=majority`;
-console.log(uri);
+
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
   serverApi: {
@@ -18,11 +21,25 @@ const client = new MongoClient(uri, {
 
 async function run() {
   try {
-    // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
-    // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+   
+   client.connect();
+  
+    const BookCollection = client.db('Books').collection('data');
+    
+     app.get('/books', async (req, res) => {
+      const cursor = BookCollection.find({});
+      const book = await cursor.toArray();
+
+      res.send(book);
+     });
+    
+    app.get('/book/:id', async (req, res) => {
+      const id = req.params.id;
+      console.log(id, "id");
+     const result = await BookCollection.findOne({ _id: new ObjectId(id) });
+      console.log(result, "result");
+      res.send(result);
+    });
   } finally {
     // Ensures that the client will close when you finish/error
     await client.close();
